971ba45e6dab20b3ba8468e2cece313a99c58c1b
BruceP <brucepark0@gmail.com>
2013-08-23 14:21:30 -0700

    chapter1-ex1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,7 @@ module Rulers
   # Your code goes here...
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
       ["Hello from Ruby on Rulers!"]]
     end
