80e00dd6b12d02e1264d3f405b721af37ed68132
BruceP <brucepark0@gmail.com>
2013-08-23 17:16:27 -0700

    finished chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,28 @@
 require "rulers/version"
-require "rulers/array"
+require "rulers/routing"
 module Rulers
   # Your code goes here...
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-      ["Hello from Ruby on Rulers!"]]
+      [text]]
     end
   end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
+    end
+  end
+
 end
