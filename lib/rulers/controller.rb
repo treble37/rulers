8b8beba76809eabed44ad173dae70842eca6a6ce
BruceP <brucepark0@gmail.com>
2013-09-11 12:22:35 -0700

    exercises & tests

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -9,19 +9,17 @@ module Rulers
     def env
       @env
     end
-  end
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub /Controller$/, ""
+      Rulers.to_underscore klass
+    end
 
-  def controller_name
-    klass = self.class
-    klass = klass.to_s.gsub /Controller$/, ""
-    Rulers.to_underscore klass
+    def render(view_name, locals = {})
+      filename = File.join "app", "views",controller_name,"#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result locals.merge(:env => env, :rulers_version => Rulers::VERSION)
+    end
   end
-
-  def render(view_name, locals = {})
-    filename = File.join "app", "views",controller_name,"#{view_name}.html.erb"
-    template = File.read filename
-    eruby = Erubis::Eruby.new(template)
-    eruby.result locals.merge(:env => env)
-  end
-  
 end
--- test/test_application.rb
+++ test/test_application.rb
@@ -1,5 +1,13 @@
 require_relative "test_helper"
+class TestController < Rulers::Controller
+  def index
+    "Hello!" # Not rendering a view
+  end
+end
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 class RulersAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
@@ -7,7 +15,7 @@ class RulersAppTest < Test::Unit::TestCase
     TestApp.new
   end
   def test_request
-    get "/"
+    get "/example/route"
     assert last_response.ok?
     body = last_response.body
     assert body["Hello"]
