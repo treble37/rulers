75a91a43667bbd5cee06cd456bf34b4c089c9893
BruceP <brucepark0@gmail.com>
2013-09-26 21:51:28 -0700

    chapter 6 reading

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -22,7 +22,12 @@ module Rulers
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-        [200, {'Content-Type' => 'text/html'},[text]]
+        if controller.get_response
+          st, hd, rs = controller.get_response.to_a
+          [st, hd, [rs.body].flatten]
+        else
+          [200,{'Content-Type' => 'text/html'}, [text]]
+        end
       rescue
         [500, {'Content-Type' => 'text/html'},["simple error - no controller action - klass: #{klass.inspect} action: #{act.inspect} controller: #{controller.inspect}"]]
       end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require "erubis"
 require "rulers/file_model"
+require "rack/request"
 # rulers/lib/rulers/controller.rb
 module Rulers
   class Controller
@@ -21,6 +22,23 @@ module Rulers
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env, :rulers_version => Rulers::VERSION)
+    end
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+    def params
+      request.params
+    end
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status,headers)
+    end
+    def get_response # Only for Rulers
+      @response
+    end
+    def render_response(*args)
+      response(render(*args))
     end
   end
 end
