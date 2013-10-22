c2184c9b156a44bc6b0b97f20640ec9d33891bf7
BruceP <brucepark0@gmail.com>
2013-10-21 21:39:04 -0700

    rebuilding rails p. 124

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,11 +1,28 @@
 # rulers/lib/rulers/routing.rb
 module Rulers
+  class RouteObject
+    def initialize
+    @rules = []
+    end
+    def match(url, *args)
+    end
+    def check_url(url)
+    end
+  end
   class Application
     def get_controller_and_action(env)
       _, cont, action, after = env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize # "People"
       cont += "Controller" # "PeopleController"
       [Object.const_get(cont), action]
+    end
+    def route(&block)
+      @route_obj ||= RouteObject.new
+      @route_obj.instance_eval(&block)
+    end
+    def get_rack_app(env)
+      raise "No routes!" unless @route_obj
+      @route_obj.check_url env["PATH_INFO"]
     end
   end
 end
